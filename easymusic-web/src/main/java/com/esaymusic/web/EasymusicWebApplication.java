c9a2cc207fa289f460a3082528bf73ba9172a69d
CinGin <1305709592@qq.com>
2019-04-29 12:09:32 +0800

    easymusic-web_v1(初始版本)

---
+++ easymusic-web/src/main/java/com/esaymusic/web/EasymusicWebApplication.java
@@ -0,0 +1,13 @@
+package com.esaymusic.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymusicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymusicWebApplication.class, args);
+	}
+
+}
